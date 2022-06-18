516cef0e93d6e0f5ddca21411c0c520bc9a47463
Volodymyr Besida <volodymyrbesida@gmail.com>
2022-06-18 13:50:12 +0300

    fixed output and removed unnecessary variables

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,3 @@
 output webapp_url {
-  value       = "data.azurerm_linux_web_app.webapp.default_site_hostname"
+  value       = azurerm_linux_web_app.webapp.name
 }
-
-output webapp_ips {
-  value       = "data.azurerm_linux_web_app.webapp.outbound_ip_addresses"
-}
-
-output postgresql_flexible_server_id {
-  value       = "azurerm_postgresql_flexible_server.postgre-flexible-server.id"
-}
-
-output postgre_flexible_server_database_id {
-  value       = "data.azurerm_postgresql_flexible_server_database.postgre-flexible-server.id"
-}
-
